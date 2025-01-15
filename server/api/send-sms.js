a132f14fc7743a4938a70c4990561f73e9851040
YATACHI <60470788+p2510@users.noreply.github.com>
2025-01-16 00:46:18 +0100

    update

--- server/api/send-sms.js
+++ server/api/send-sms.js
@@ -2,33 +2,5 @@ import { defineEventHandler } from "h3"; // Utilisation de h3 pour Nuxt 3
 import { serverSupabaseClient } from "#supabase/server"; // Si tu veux utiliser Supabase pour autre chose
 import { MailtrapClient } from "mailtrap";
 export default defineEventHandler(async (event) => {
-
-  // Looking to send emails in production? Check out our Email API/SMTP product!
-  const TOKEN = "93ee75277c7d12de359629e0c3495b7e";
-
-  const client = new MailtrapClient({
-    token: TOKEN,
-    testInboxId: 2094641,
-  });
-
-  const sender = {
-    email: "hello@example.com",
-    name: "Mailtrap Test",
-  };
-  const recipients = [
-    {
-      email: "poupoinaka03@gmail.com",
-    },
-  ];
-
-  client.testing
-    .send({
-      from: sender,
-      to: recipients,
-      subject: "You are awesome!",
-      text:'cc fr ',
-      category: "Integration Test",
-    })
-    .then(console.log, console.error);
-  return "good";
+  return { merci: "cqsd" };
 });
