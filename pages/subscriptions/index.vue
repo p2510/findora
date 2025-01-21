990b6e4a0ab8314a597d9228c82bfd8b5fd4c23c
YATACHI <60470788+p2510@users.noreply.github.com>
2025-01-21 22:51:24 +0100

    update

--- pages/subscriptions/index.vue
+++ pages/subscriptions/index.vue
@@ -16,7 +16,7 @@
     </p>
     <section class="grid grid-cols-12 gap-2">
       <div
-        class="col-span-4 lg:col-start-2 lg:col-end-5 bg-transparent border-[1px] border-slate-200 h-screen rounded-xl"
+        class="col-span-4 lg:col-span-4 bg-transparent border-[1px] border-slate-200 h-screen rounded-xl"
       >
         <p class="text-center py-4 text-lg text-slate-600">Free</p>
         <p class="flex justify-center items-baseline gap-x-2">
@@ -154,7 +154,7 @@
         </ul>
       </div>
       <div
-        class="col-span-4 lg:col-start-5 lg:col-end-8 bg-transparent border-[1px] border-orange-200 h-screen rounded-xl"
+        class="col-span-4 lg:col-span-4 bg-transparent border-[1px] border-orange-200 h-screen rounded-xl"
       >
         <p class="text-center py-4 text-lg text-slate-600">Premium</p>
         <p class="flex justify-center items-baseline gap-x-2">
@@ -296,48 +296,145 @@
         </div>
       </div>
       <div
-        class="hidden col-start-9 col-end-13 bg-transparent lg:flex items-center h-screen"
+        class="col-span-4 lg:col-span-4 bg-transparent border-[1px] border-orange-200 h-screen rounded-xl"
       >
-        <div
-          class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+        <p class="text-center py-4 text-lg text-slate-600">Premium</p>
+        <p class="flex justify-center items-baseline gap-x-2">
+          <span class="text-5xl font-semibold tracking-tight text-gray-900"
+            >70.000F</span
+          >
+          <span class="text-base text-gray-500">/an</span>
+        </p>
+        <p class="mt-6 text-gray-600 text-sm px-3 text-center">
+          Économisez l'équivalent un mois en optant dès maintenant pour
+          l'abonnement annuel !
+        </p>
+        <ul
+          role="list"
+          class="px-3 mt-8 space-y-3 text-sm/6 text-gray-600 sm:mt-10"
         >
-          <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-            <div class="sm:flex sm:items-start">
-              <div
-                class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:size-10"
-              >
-                <svg
-                  class="size-6 text-green-600"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                  data-slot="icon"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
-                  />
-                </svg>
-              </div>
-              <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                <h3
-                  class="text-base font-semibold text-gray-900"
-                  id="modal-title"
-                >
-                  Paiement par wave
-                </h3>
-                <div class="mt-2">
-                  <p class="text-sm text-gray-500">
-                    Une fois que votre paiement effectué votre abonnement est
-                    activé dans un délai de 2h maximum .
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <li class="flex gap-x-3">
+            <svg
+              class="h-6 w-5 flex-none text-[#f3c775]"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            clients illimités
+          </li>
+          <li class="flex gap-x-3">
+            <svg
+              class="h-6 w-5 flex-none text-[#f3c775]"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            paiements illimités
+          </li>
+          <li class="flex gap-x-3">
+            <svg
+              class="h-6 w-5 flex-none text-[#f3c775]"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            1 500 sms ( Compte personnalisé)
+          </li>
+          <li class="flex gap-x-3">
+            <svg
+              class="h-6 w-5 flex-none text-[#f3c775]"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            Relance automatique
+          </li>
+
+          <li class="flex gap-x-3">
+            <svg
+              class="h-6 w-5 flex-none text-[#f3c775]"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            Création de template
+          </li>
+          <li class="flex gap-x-3">
+            <svg
+              class="h-6 w-5 flex-none text-[#f3c775]"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            Campagne client
+          </li>
+          <li class="flex gap-x-3">
+            <svg
+              class="h-6 w-5 flex-none text-[#f3c775]"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+
+            Support client
+          </li>
+        </ul>
+        <div class="flex justify-center mt-6">
+          <a
+            href="https://pay.wave.com/m/M_ci_KZ66i6-Hu0Q2/c/ci/?amount=70000"
+            target="_blank"
+            class="bg-[#f3c775] hover:bg-[#ddab4e] rounded-full shadow-md text-md py-3 px-6 transition-all duration-300 ease-in-out"
+            >Passer premium</a
+          >
         </div>
       </div>
     </section>
