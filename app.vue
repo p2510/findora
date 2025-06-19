09b8d519719bd1a9321246458bb2d61b1ece3077
POUPOINA AKA <60470788+p2510@users.noreply.github.com>
2025-06-19 13:27:56 +0200

    update

--- app.vue
+++ app.vue
@@ -10,16 +10,26 @@
 import { computed,watch } from "vue";
 import { useRoute } from "#app";
 const { initialize } = useHotjar()
-/*
 useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover' },
+    { name: 'apple-mobile-web-app-capable', content: 'yes' },
+    { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+    { name: 'mobile-web-app-capable', content: 'yes' },
+    { name: 'theme-color', content: '#ffffff' },
+    { name: 'apple-mobile-web-app-title', content: 'Findora' }
+  ],
+  link: [
+    { rel: 'manifest', href: '/manifest.json' }
+  ],
+  /*
   script: [
     {
       src: "//code.tidio.co/ew6xfoks6xdz4jku5masrytmyvtk6ygm.js",
       async: true,
     },
-  ],
+  ],*/
 });
-*/
 const route = useRoute();
 const listRoute = ["/","/en", "/confirm"];
 const shouldHideLayout = computed(() => listRoute.includes(route.path));
